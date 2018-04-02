cf12fdae906515f766bef4eccb83a6a2901d30e3
petegaggs <35112776+petegaggs@users.noreply.github.com>
2018-04-02 17:26:05 +0100

    Add files via upload
    MK2 hardware is an attempt to make it a bit simpler/cheaper.
    Uses 8 bit PWM instead of SPI DAC to drive the integrator. However, note that the range is not as large, Top midi note is now 84 (c-6). Note above this will play, but the amplitude will fall.
    Also, integrator is a different design, does not need a negative supply rail.
    software supports both versions of hardware, use the #define MK1hHW or MK2HW as appropriate

--- synth_controller.ino
+++ synth_controller.ino
@@ -2,7 +2,10 @@
  * synth controller, by Peter Gaggs
  * 16 bit timer is used to generate a square wave at the required frequency
  * this is used to reset the integrator (ramp generator)
- * the drive for the integrator is provided by a 12bit SPI DAC
+ * 2 versions of hardware are supported:
+ * MK1: 12 bit SPI dac for integrator drive
+ * MK2: 8 bit PWM, lower cost, for integrator drive, does not have quite the same note range
+ * (optionally this can come from PWM instead, but with a reduced note range available)
  * runs on arduino nano or similar 328p
  * note we use the timer directly, hence only use on 328p or 168p
  * needs arduino MIDI library
@@ -14,24 +17,31 @@
 #include <midi_Settings.h> 
 MIDI_CREATE_DEFAULT_INSTANCE();
 #include <avr/io.h> 
+#include <avr/interrupt.h>
 #include <SPI.h>
+#define MK1HW // define this for orginal version 1 hardware
+//#define MK2HW // define this for version 2 hardware
 #define SLAVE_SELECT_PIN 10 //spi chip select
 #define TIMER_PIN 9 //OC1A output
+#define INTEGRATOR_RESET_PIN 12 //short pulse to reset integrator, used in mk2 h/w
+#define INTEGRATOR_PWM_PIN 3 //integrator drive by timer2 PWM OC2B, for mk2 h/w
 #define GATE_PIN 8 //gate control
 #define VOLTAGE_CONTROL_PIN A0
 #define MIDI_BASE_NOTE 21 //lowest midi note
 #define BASE_NOTE_FREQ 27.5
 #define VOLTS_PER_SEMITONE 1.0 / 12.0
-#define DAC_MULTIPLIER 0.97783686 // see spreadsheet
+#define SPI_DAC_MULTIPLIER 0.97783686 // see spreadsheet
+#define PWM_DAC_MULTIPLIER 0.24 // used for mk2 h/w
 #define PITCH_BEND_FACTOR 1.0 / 32768.0; //adjust for desired pitch bend operation
 #define ANALOG_CONTROL_SCALING 1.0 / 512.0 //adjust for desired amount of analog control
-#define ANALOG_CONTROL // comment this out if analogue control is not required
+//#define ANALOG_CONTROL // comment this out if analogue control is not required
 //MIDI variables
 int currentMidiNote; //the note currently being played
 int keysPressedArray[128] = {0}; //to keep track of which keys are pressed
 float midiControlVoltage; // represents midi note
 float analogControlVoltage = 0; // analog control from ADC (optional)
 float bendControlVoltage = 0; // represents midi pitch bend
+bool oddEvenIntCnt = false;
 
 void setTimer1(uint16_t val) {
   OCR1AH = val >> 8;
@@ -49,15 +59,28 @@ void setup() {
   // and also pitchbend, we can do that now we calculate the value on the fly
   MIDI.setHandlePitchBend(handlePitchBend); 
   //SPI stuff
+  #ifdef MK1HW
   pinMode (SLAVE_SELECT_PIN, OUTPUT);
   digitalWrite(SLAVE_SELECT_PIN,HIGH);
   SPI.begin();
+  #endif
   pinMode(TIMER_PIN,OUTPUT);// OC1A output
   pinMode(GATE_PIN,OUTPUT);
   digitalWrite(GATE_PIN,LOW);
-  // fast PWM mode 15, x8 prescaling
+  // timer 1 fast PWM mode 15, x8 prescaling
   TCCR1A = _BV(COM1A0) | _BV(WGM11) | _BV(WGM10);
   TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11);
+ 
+  #ifdef MK2HW
+  pinMode(INTEGRATOR_RESET_PIN,OUTPUT);// short pulse to reset integrator
+  pinMode(INTEGRATOR_PWM_PIN, OUTPUT); // OC2B output
+  // timer 1 interrupts.
+  TIMSK1 = _BV(TOIE1);
+  // timer 2 fast pwm, no prescaling, non inverting mode channel B
+  TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS20);
+  #endif
+
   setNotePitch(60); //middle C for test
 }
 
@@ -67,6 +90,21 @@ void loop() {
   readAnalogControlVoltage();
   updateNotePitch();
   #endif
+}
+
+SIGNAL(TIMER1_OVF_vect)
+{
+  // ISR for timer 1: generate short pulse to reset integrator
+  // only generate pulse every other time interrup is called
+  // since there will be 2 interrupts per note period
+  if (oddEvenIntCnt) {
+    PORTB |= 0x10; // set PB4 (arduino pin 12)
+    oddEvenIntCnt = false;
+    delayMicroseconds(10); // give a bit of time to discharge capacitor
+    PORTB &= ~0x10; // clear PB1 (arduino pin 9)    
+  } else {
+    oddEvenIntCnt = true;
+  }
 }
 
 void readAnalogControlVoltage() {
@@ -113,13 +151,27 @@ void handlePitchBend (byte channel, int bend) {
   updateNotePitch();
 }
 
+void oscSetPwm (float freqHz) {
+  // set PWM value according to note frequency, timer 2 channel B used for osc integrator drive
+  int pwm_val = round(PWM_DAC_MULTIPLIER * freqHz);
+  if (pwm_val > 255) {
+    pwm_val = 255;
+  }
+  OCR2B = pwm_val;
+}
+
 void updateNotePitch() {
   // update note pitch, taking into account midi note, midi pitchbend, analogue control
   float controlVoltage = midiControlVoltage + bendControlVoltage + analogControlVoltage;
   float freqHz = BASE_NOTE_FREQ * pow(2.0, controlVoltage);  
   uint16_t timerSetting = round((1000000.0 / freqHz)-1.0);
   setTimer1(timerSetting);
-  dacWrite(round(DAC_MULTIPLIER * freqHz));
+  #ifdef MK1HW
+  dacWrite(round(SPI_DAC_MULTIPLIER * freqHz));
+  #endif
+  #ifdef MK2HW
+  oscSetPwm(freqHz);
+  #endif
 }
 
 void setNotePitch(int note) {
