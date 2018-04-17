8e7a8f4a56d583163b1680bdb57560dd810eaf85
petegaggs <35112776+petegaggs@users.noreply.github.com>
2018-04-17 20:36:27 +0100

    Add files via upload
    added noise output pin, and sub oscillator output (square wave one octave below main oscillator)

--- synth_controller.ino
+++ synth_controller.ino
@@ -19,19 +19,21 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 #include <avr/io.h> 
 #include <avr/interrupt.h>
 #include <SPI.h>
-#define MK1HW // define this for orginal version 1 hardware
-//#define MK2HW // define this for version 2 hardware
+//#define MK1HW // define this for orginal version 1 hardware
+#define MK2HW // define this for version 2 hardware
 #define SLAVE_SELECT_PIN 10 //spi chip select
 #define TIMER_PIN 9 //OC1A output
 #define INTEGRATOR_RESET_PIN 12 //short pulse to reset integrator, used in mk2 h/w
 #define INTEGRATOR_PWM_PIN 3 //integrator drive by timer2 PWM OC2B, for mk2 h/w
 #define GATE_PIN 8 //gate control
+#define NOISE_PIN 7 //psuedo random noise output
+#define SUB_OSC_PIN 6 //sub oscillator - square wave one octave below main osc
 #define VOLTAGE_CONTROL_PIN A0
 #define MIDI_BASE_NOTE 21 //lowest midi note
 #define BASE_NOTE_FREQ 27.5
 #define VOLTS_PER_SEMITONE 1.0 / 12.0
 #define SPI_DAC_MULTIPLIER 0.97783686 // see spreadsheet
-#define PWM_DAC_MULTIPLIER 0.24 // used for mk2 h/w
+#define PWM_DAC_MULTIPLIER 0.2 // used for mk2 h/w
 #define PITCH_BEND_FACTOR 1.0 / 32768.0; //adjust for desired pitch bend operation
 #define ANALOG_CONTROL_SCALING 1.0 / 512.0 //adjust for desired amount of analog control
 //#define ANALOG_CONTROL // comment this out if analogue control is not required
@@ -42,6 +44,7 @@ float midiControlVoltage; // represents midi note
 float analogControlVoltage = 0; // analog control from ADC (optional)
 float bendControlVoltage = 0; // represents midi pitch bend
 bool oddEvenIntCnt = false;
+uint32_t lfsr = 1; //32 bit LFSR, must be non-zero to start
 
 void setTimer1(uint16_t val) {
   OCR1AH = val >> 8;
@@ -66,6 +69,8 @@ void setup() {
   #endif
   pinMode(TIMER_PIN,OUTPUT);// OC1A output
   pinMode(GATE_PIN,OUTPUT);
+  pinMode(NOISE_PIN,OUTPUT);
+  pinMode(SUB_OSC_PIN,OUTPUT);
   digitalWrite(GATE_PIN,LOW);
   // timer 1 fast PWM mode 15, x8 prescaling
   TCCR1A = _BV(COM1A0) | _BV(WGM11) | _BV(WGM10);
@@ -76,9 +81,12 @@ void setup() {
   pinMode(INTEGRATOR_PWM_PIN, OUTPUT); // OC2B output
   // timer 1 interrupts.
   TIMSK1 = _BV(TOIE1);
-  // timer 2 fast pwm, no prescaling, non inverting mode channel B
-  TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  // timer 2 phase accurate PWM, no prescaling, non inverting mode channel B
+  //TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2A = _BV(COM2B1) | _BV(WGM20);
   TCCR2B = _BV(CS20);
+  // timer 2 interrupts (used for noise)
+  TIMSK2 = _BV(TOIE2);
   #endif
 
   setNotePitch(60); //middle C for test
@@ -92,16 +100,35 @@ void loop() {
   #endif
 }
 
+SIGNAL(TIMER2_OVF_vect) {
+  // noise output on digital pin 7 = PD7
+  unsigned lsb = lfsr & 1;
+  if (lsb) {
+    PORTD |= 0x80;
+  }
+  else {
+    PORTD &= ~0x80;
+  }
+  // advance LFSR
+  lfsr >>= 1;
+  if (lsb) {
+    lfsr ^= 0xA3000000u; // 32 bit version
+  }  
+}
+
+
 SIGNAL(TIMER1_OVF_vect)
 {
   // ISR for timer 1: generate short pulse to reset integrator
   // only generate pulse every other time interrup is called
   // since there will be 2 interrupts per note period
+  // also handle sub oscillator here by toggling pin
   if (oddEvenIntCnt) {
-    PORTB |= 0x10; // set PB4 (arduino pin 12)
+    PORTB |= 0x10; // set PB4 (arduino pin 12) integrator reset
+    PORTD ^= 0x40; // toggle PD6 sub osc pin
     oddEvenIntCnt = false;
     delayMicroseconds(10); // give a bit of time to discharge capacitor
-    PORTB &= ~0x10; // clear PB1 (arduino pin 9)    
+    PORTB &= ~0x10; // clear integrator reset
   } else {
     oddEvenIntCnt = true;
   }
